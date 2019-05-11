7275209175c90797c1a743a553c96b3009838822
tanialopezab <tania.lopez.ab@gmail.com>
2019-05-11 20:59:31 +0200

    create build folder

---
+++ docs/precache-manifest.e7485ed4c49d78697b037681f4cd0806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4d906d950bd1d497a76e7a5998072c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a4bd2e424f833f3a04c",
+    "url": "./static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "./static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2a4bd2e424f833f3a04c",
+    "url": "./static/js/main.643ebfc4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
